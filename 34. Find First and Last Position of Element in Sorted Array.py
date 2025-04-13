db596b7b69e334a06de1785c6e71316d9bb0bf55
Uthej Karamalapudi <121709203+UthejKaramalapudi@users.noreply.github.com>
2025-04-13 10:46:21 +0530

    Update 34. Find First and Last Position of Element in Sorted Array.py

--- 34. Find First and Last Position of Element in Sorted Array.py
+++ 34. Find First and Last Position of Element in Sorted Array.py
@@ -1,26 +1,36 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = self.binSearch(nums, target, True)
-        right = self.binSearch(nums, target, False)
+        
+        def first_occur(nums, target):
+            n = len(nums)
+            l, r = 0, n - 1
+            first = -1
 
-        return [left, right]
-
-    def binSearch(self, nums, target, left_bias):
-        l, r = 0, len(nums) - 1
-        i = -1
-
-        while l <= r:
-            m = (l + r) // 2
-
-            if target > nums[m]:
-                l = m + 1
-            elif target < nums[m]:
-                r = m - 1
-            else:
-                i = m
-                if left_bias:
-                    r = m - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] == target:
+                    first = mid
+                    r = mid - 1
+                elif nums[mid] < target:
+                    l = mid + 1
                 else:
-                    l = m + 1
+                    r = mid - 1
+            return first
 
-        return i
+        def last_occur(nums, target):
+            n = len(nums)
+            l, r = 0, n - 1
+            last = -1
+
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] == target:
+                    last = mid
+                    l = mid + 1
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return last
+
+        return [first_occur(nums, target), last_occur(nums, target)]
